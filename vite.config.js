fb63980a3f591ed72f43892f658abe44342c7278
Peter Barredo <pvbarredo@gmail.com>
2026-02-20 16:16:23 +0800

    vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/opi-highlight-app/',
+  base: process.env.GITHUB_PAGES === 'true' ? '/opi-highlight-app/' : '/',
 })
